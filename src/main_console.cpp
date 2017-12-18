3e3076e226fc372eae403e2e11d1ab9ef7aa8932
Daneel Brychikov <BrychDaneel@mail.ru>
2017-12-18 23:25:37 +0300

    Add console parametrs.

--- src/main_console.cpp
+++ src/main_console.cpp
@@ -3,6 +3,8 @@
 #include <easylogging++/easylogging++.h>
 
 #include <client/TCPTrainClient.h>
+#include <cstdlib>
+#include <iostream>
 
 
 using namespace tiger::trains;
@@ -13,7 +15,27 @@ INITIALIZE_EASYLOGGINGPP
 
 int main(int argc, char *argv[]){
 
-    Runner runner("Tiger", "wgforge-srv.wargaming.net", 443);
+    const char * login = "Tiger";
+    const char * host = "wgforge-srv.wargaming.net";
+    int port = 443;
+
+    if (argc > 1){
+        if (argc > 4){
+            std::cerr << "Wrong number of arguments." << std::endl;
+            std::cerr << "Usage:  console (login) (host) (port)" << std::endl;
+            return -1;
+        }
+        login = argv[1];
+        host = argv[2];
+        port = atoi(argv[3]);
+
+        if (!port){
+            std::cerr << "Invalid port." << std::endl;
+            return -1;
+        }
+    }
+
+    Runner runner(login, host, port);
     ai::ComplexBot complexBot;
     runner.setBot(&complexBot);
 
